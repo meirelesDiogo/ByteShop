830e003041b84298aba65bdb408e1fe1a1fb9af5
Diogo Alexandre <didialexandre1017@gmail.com>
2026-07-18 16:59:59 -0300

    phpMailer adicionado

--- contato.php
+++ contato.php
@@ -36,8 +36,62 @@ if($_POST){
     $email=$_POST['email'];
     $mensagem=$_POST['mensagem'];
 
+    try{
+        $mail = require 'config/mail.php';
+
+        $mail = require 'config/mail.php';
+        // ==========================
+        // EMAIL PARA O CLIENTE
+        // ==========================
+
+        $mail->clearAddresses();
+
+        $mail->addAddress($email, $nome);
+
+        $mail->Subject = "Recebemos seu contato";
+
+        $mail->Body = "
+
+        <div style='font-family:Arial;max-width:600px;margin:auto;padding:20px;border:1px solid #eee;border-radius:10px;'>
+
+            <h2 style='color:#1e66ff'>
+                Olá, {$nome}!
+            </h2>
+
+            <p>
+                Recebemos sua mensagem com sucesso.
+            </p>
+
+            <p>
+                Nossa equipe já foi notificada e responderá o mais rápido possível.
+            </p>
+
+            <hr>
+
+            <h3>Sua mensagem</h3>
+
+            <p>{$mensagem}</p>
+
+            <br>
+
+            <b>ByteShop</b>
+
+        </div>
+
+        ";
+
+        $mail->send();
+
+        $sucesso = true;
+
+    } catch (Exception $e) {
+
+        $erro = $e->getMessage();
+
+    }
+    }
         
-}
+
 ?>
 </body>
 </html>
